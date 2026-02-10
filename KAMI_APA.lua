1cc5e15d2ba0cfb39f96f276845bebbc42e5af98
Tryitus <betabetalg@gmail.com>
2026-02-11 02:50:54 +0700

    Increase MAX_SPAWN_BEFORE_FORGET from 12 to 15

--- KAMI_APA.lua
+++ KAMI_APA.lua
@@ -14,7 +14,7 @@ getgenv().TARGET_LIST = getgenv().TARGET_LIST or {}
 getgenv().FORGOTTEN_UNITS = {}
 getgenv().UNIT_SPAWN_COUNT = {}
 getgenv().SEEN_UNIT_INSTANCES = {}
-getgenv().MAX_SPAWN_BEFORE_FORGET = 12
+getgenv().MAX_SPAWN_BEFORE_FORGET = 15
 
 getgenv().GRAB_RADIUS = 10
 getgenv().TARGET_TIMEOUT = 30
