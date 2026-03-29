0c530870d1eebbee8119647f3ff047043dc6d969
Tryitus <betabetalg@gmail.com>
2026-03-30 01:01:23 +0700

    Update KAMI_APA.lua

--- KAMI_APA.lua
+++ KAMI_APA.lua
@@ -324,7 +324,7 @@ end)
 if not getgenv().__KAMI_APA_AUTO_RESET_RUNNING then
 
 	getgenv().__KAMI_APA_AUTO_RESET_RUNNING = true
-	local AUTO_RESET_DELAY = 240
+	local AUTO_RESET_DELAY = 1200
 
 	task.spawn(function()
 
