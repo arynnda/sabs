3267e8c73b333b587aeb9ce5683bf2cee2184080
Tryitus <betabetalg@gmail.com>
2026-04-09 15:00:51 +0700

    Update Fps.lua

--- Fps.lua
+++ Fps.lua
@@ -5,7 +5,7 @@ task.spawn(function()
 end)
 
 getgenv().BlackModeConfig = {
-    enableBlackOverlay = true,
+    enableBlackOverlay = false,
     overlayZIndex = 100000,
     overlayTransparency = 0,
     reduceGraphics = true,
