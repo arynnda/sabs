94a308de5b49bac884bfdd161c0846e3024c002d
Tryitus <betabetalg@gmail.com>
2026-04-28 13:44:13 +0700

    Update KAMI_APA.lua

--- KAMI_APA.lua
+++ KAMI_APA.lua
@@ -18,7 +18,7 @@ getgenv().SEEN_UNIT_INSTANCES = {}
 
 getgenv().MAX_SPAWN_BEFORE_FORGET = 6
 
-getgenv().GRAB_RADIUS = 25
+getgenv().GRAB_RADIUS = 12
 getgenv().TARGET_TIMEOUT = 50
 getgenv().CHASE_DELAY = 0.5
 
