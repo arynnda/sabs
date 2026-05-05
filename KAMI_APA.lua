c1f0b01a38f28249423358d9de1ed8fb28e5482a
Tryitus <betabetalg@gmail.com>
2026-05-06 04:15:20 +0700

    Update KAMI_APA.lua

--- KAMI_APA.lua
+++ KAMI_APA.lua
@@ -334,7 +334,7 @@ if not getgenv().__KAMI_APA_AUTO_BUY_FIX then
 					getgenv().IS_INTERACTING = true
 
 					pcall(function()
-						fireproximityprompt(v, 1)
+						fireproximityprompt(v, 0)
 					end)
 
 					task.wait(0.2)
