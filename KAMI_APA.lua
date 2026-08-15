359f2013775cf36ddd72f09094a88288152a5731
Tryitus <betabetalg@gmail.com>
2026-08-16 03:18:12 +0700

    Update KAMI_APA.lua

--- KAMI_APA.lua
+++ KAMI_APA.lua
@@ -219,7 +219,6 @@ end)
 local TARGETS = {
 	Vector3.new(-410.9753, -6.50, 71.84),
 	Vector3.new(-436.8611, -6.25, 64.40),
-	Vector3.new(-410.9753, -6.50, 71.84),	
 }
 
 local ARRIVE_DISTANCE = 5
@@ -270,7 +269,7 @@ local function startSystem()
 			if not reached then return end
 		end
 
-		local lastTarget = TARGETS[3]
+		local lastTarget = TARGETS[2]
 
 		local startWait = tick()
 
